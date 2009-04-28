894af7b5ae6ab432ad5f791a19694aa3399085e4
Gareth Townsend <quamen@gmail.com>
2009-04-28 20:31:15 +1000

    Automatically strip all params that are not required by rails routing to operate.
    Re-insert params defined as allowable by allow_assignment method call in controller.

--- lib/bouncer.rb
+++ lib/bouncer.rb
@@ -1,7 +1,9 @@
 # Bouncer
 
 class ActionController::Base
-  def self.allow_assignment(*assignable_attributes_hash)
+  before_filter :cache_params_hash
+  
+  def self.allow_assignment(*assignable_attributes_hash)    
     assignable_attributes_hash.each do |attribute_hash|
       attribute_hash.keys.each do |key|
         before_filter { |controller| controller.send(:slice_attributes_for, key, attribute_hash[key]) }
@@ -10,8 +12,18 @@ class ActionController::Base
   end
   
   private
+  
+  def cache_params_hash
+    @cached_params_hash = params.dup
+    keys = [:authenticity_token, :_method]
+    keys += self.request.env['rack.routing_args'].keys
+    params.slice!(*keys)
+  end
 
   def slice_attributes_for(params_hash_symbol, assignable_attributes)
-    params[params_hash_symbol].slice!(*assignable_attributes) if params[params_hash_symbol]
+    if @cached_params_hash[params_hash_symbol]
+      allowed_attributes = @cached_params_hash[params_hash_symbol].slice(*assignable_attributes) 
+      params[params_hash_symbol] = allowed_attributes
+    end
   end
 end
